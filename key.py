4333f98edc96828fba802fbea2b67c1a0f64fa88
Isha <imasada@vtcap.org>
2020-12-15 22:06:58 -0500

    deleted

--- key.py
+++
@@ -1,8 +0,0 @@
-class Key:
-	
-	def __repr__(self):
-		return '{scale}'.format(scale = self.scale)
-		
-	def __init__(self, scale):
-		self.scale = scale
-		self.third = scale[0], scale[2], scale[4]
