797bc42928bba1616ed33a2f8bee510cbf454a43
Isha <imasada@vtcap.org>
2020-12-23 10:33:47 -0500

    used jake fixing to add the add tone to the chord

--- chord_seq.py
+++ chord_seq.py
@@ -15,35 +15,39 @@ class Key:
     
 SCALE = {
     'a minor': ['A', 'B', 'C', 'D', 'E', 'F', 'G'] ,
-    'a major': ['A', 'B', 'CS', 'D', 'E', 'FS', 'GS'],  
-    'b minor': ['B', 'CS', 'D', 'E', 'FS', 'G', 'A'],
-    'b major': ['B', 'CS', 'D', 'E', 'FS', 'GS', 'AS'],
-    'c minor': ['C', 'D', 'DS', 'F', 'G', 'GS', 'AS'],
+    'a major': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'],  
+    'b minor': ['B', 'C#', 'D', 'E', 'F#', 'G', 'A'],
+    'b major': ['B', 'C#' 'D', 'E', 'F#', 'G#', 'A#'],
+    'c minor': ['C', 'D', 'D#', 'F', 'G', 'GS', 'AS#'],
     'c major': ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
-    'd minor': ['D', 'E', 'F', 'G', 'A', 'AS', 'C'],
-    'd major': ['D', 'E', 'FS', 'G', 'A', 'B', 'CS'],
-    'e minor': ['E', 'FS', 'G', 'A', 'B', 'C', 'D'],
-    'e major': ['E', 'FS', 'GS', 'A', 'B', 'CS', 'DS'],
-    'f minor': ['F', 'G', 'GS', 'AS', 'C', 'CS', 'DS'],
-    'f major': ['F', 'G', 'A', 'AS', 'C', 'D', 'E'],
-    'g minor': ['G', 'A', 'AS', 'C', 'D', 'DS', 'F'],
-    'g major': ['G', 'A', 'B', 'C', 'D', 'E', 'FS']
+    'd minor': ['D', 'E', 'F', 'G', 'A', 'A#', 'C'],
+    'd major': ['D', 'E', 'F#', 'G', 'A', 'B', 'C#'],
+    'e minor': ['E', 'F#', 'G', 'A', 'B', 'C', 'D'],
+    'e major': ['E', 'F#', 'G#', 'A', 'B', 'C#', 'D#'],
+    'f minor': ['F', 'G', 'G#', 'A#', 'C', 'C#', 'D#'],
+    'f major': ['F', 'G', 'A', 'A#', 'C', 'D', 'E'],
+    'g minor': ['G', 'A', 'A#', 'C', 'D', 'D#', 'F'],
+    'g major': ['G', 'A', 'B', 'C', 'D', 'E', 'F#']
 }
 
 #get chord from user 
-scale = input('Enter the name of the chord you want: ').lower().strip()
-#separate segments of chord 
-chord = scale.split()
-chord.append('')
-scale = Key(SCALE[scale])
-#get extra interval from user 
-add_tone = input('\nEnter any add tones: ').lower().strip()
+scale = input('Enter the name of the chord you want: ').lower().strip().split()
+chord, add_tone = ' '.join(scale[:2]), scale[2:]
+#get scale from SCALE dictionary 
+scale = Key(SCALE[chord])
 #DICTIONARY for add tones 
 extra_intervals = {'seventh': scale.seventh,
             'sixth': scale.sixth,
             'ninth': scale.ninth,
             'sus': scale.sus
 }
-#finding corresponding interval between user input and extra_intervals
-add_tone = extra_intervals[add_tone]
-print(add_tone)
+#adding add tone into chord
+if add_tone:
+    # Check if additional tone is valid
+    if add_tone[0] in extra_intervals:
+        print(', '.join(extra_intervals[add_tone[0]]))
+    else:
+        print('Unrecognized additional tone "{}".'.format(add_tone[0]))
+else:
+    # Print regular third
+    print(', '.join(scale.third))
